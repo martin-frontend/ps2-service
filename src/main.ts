dce29e2d609d49ae0b804e1988ec5ddd13031e6f
penbillpopo <penbillpopo@gmail.com>
2021-01-14 11:12:06 +0800

    新增swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 // import * as cron from 'node-cron'
@@ -26,6 +27,15 @@ async function bootstrap() {
     next();
   });
 
+  //swagger
+  const options = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
